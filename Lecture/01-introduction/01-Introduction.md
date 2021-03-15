5b0cdcc2c58b433daf03b6539eb0bd0e146f3f6e
indoni <choinj97@gmail.com>
2021-03-15 23:51:15 +0900

    Studying Rxswift intro and cocoapod setting

---
+++ Lecture/01-introduction/01-Introduction.md
@@ -0,0 +1,12 @@
+# 01. Introduction
+
+### 왜 RxSwift를 쓸까
+> **Code의 대부분이 외부 Event에 대한 Response 처리**
+
+    - @IBAction handler를 쓰거나
+    - 키보드 변화 등에 대한 notification을 observation하거나
+    - URL session이 response를 내려줄 때 Clouser를 사용하거나
+    - variable의 변화를 감지하기 위해 KVO를 사용하거나
+
+
+
---
+++ Lecture/01-introduction/buttonTapTest/buttonTapTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buttonTapTest
+//
+//  Created by 최인정 on 2021/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
